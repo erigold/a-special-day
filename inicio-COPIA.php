a44b6a51f2530a4e17d3982d5907315e59212c2d
erigold <erihernandezg@live.com.mx>
2022-10-30 10:30:29 -0600

    subida2

---
+++ inicio-COPIA.php
@@ -0,0 +1,229 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+?>
+
+<!DOCTYPE html>
+
+<html lang="es-mx" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Un día especial</title>
+    <link rel="stylesheet" href="estilos.css">
+    
+</head>
+<body>
+<script src="funciones.js"></script>  
+<script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.4.0/dist/confetti.browser.min.js"></script>
+     <script> efecto_scroll();</script>
+    
+    <script type="text/javascript">mostrar();</script>
+    <section class="content">
+        <h1>Hola mi bebé....</h1>
+
+    </section>
+
+    <section class="content">
+        <div class="scroll-content fadeRight">
+            <h2>Hay una cosa que he querido preguntarte desde hace mucho tiempo...</h2>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="scroll-content fadeLeft">
+            <h3>Tu me gustas mucho y siento que tuyo y yo congeniamos muy bien</h3>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="scroll-content fadeRight">
+            <h4>Disfruto cada segundo que estoy contigo y se que tu tambien</h4>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="scroll-content fadeLeft">
+            <h5>Quiero preguntarte si.. ¿Quieres ser mi novia?</h5>
+        </div>
+    </section>
+
+<h2>Dias que llevamos de novios:
+    <label id="texto_nav1"></label> 
+    <label id="texto_nav2"></label> </h2>
+    <script>calculo_dias();</script> 
+    
+   <h2>Mira mi amoooooor</h2>
+
+<div id="txt_oculto" style="display: none;">
+<h4>Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok!
+Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok?
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok.
+Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok?
+Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok.
+Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok.
+Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok! Ok? Ok! Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok!
+Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok.
+Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok!
+Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok. Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok! Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok. Ok. Ok. Ok. Ok! Ok.
+Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok!
+Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok!
+Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok?
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok?
+Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok!
+Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok.
+Ok. Ok. Ok! Ok. Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok?
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok! Ok? Ok! Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok!
+Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok.
+Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok.
+Ok? Ok! Ok. Ok? Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok!
+Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok.
+Ok. Ok. Ok! Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok?
+Ok. </h4>
+</div>
+
+<div id="txt_oculto2" style="display: none;">
+<h4>Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok!
+Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok?
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok?
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok?
+Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok.
+Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok.
+Ok? Ok! Ok. Ok? Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok!
+Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok.
+Ok. Ok. Ok! Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok.
+Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok?
+Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok.
+Ok. Ok. Ok. Ok! Ok. Ok! Ok! Ok! Ok! Ok! Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok!
+Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok.
+Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok!
+Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok.
+Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok. Ok? Ok.
+Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok!
+Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok!
+Ok. Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok.
+Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok.
+Ok? Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok!
+Ok? Ok. Ok? Ok! Ok. Ok? Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. Ok. Ok. Ok. Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok! Ok? Ok! Ok! Ok. Ok? Ok. Ok.
+Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok? Ok. Ok? Ok! Ok.
+Ok? Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok. Ok.
+Ok! Ok. Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok. Ok. Ok! Ok. Ok!
+Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok! Ok. Ok? Ok. </h4>
+</div>    
+
+<center>
+    
+      <a href="#" onclick="cambiar('txt_oculto'); return false;">Clic aqui</a> 
+      <br>
+      <a href="#" onclick="cambiar('txt_oculto2'); return false;">¡Abreme!</a> 
+      <h3>¿Cuanto falta para cumplir un año?</h3>
+     
+<h2> <div id="countdown"></div>  </h2> 
+
+</center>
+
+ <script> contador();</script>
+
+<h1>¿Estamos mal y quieres que estemos bien tu y yo?</h1>
+<h2> ¡¡No te preocupes solo da click aquí abajo!! y veras lo que pasa</h2>
+ 
+ <center>
+      <button onclick="location.href='https://a-special-day.000webhostapp.com/send.php'">Pruebame y veras que pasa</button> 
+ </center>
+ <br>
+ <center>
+ <h2>Si cumplimos meses mira este botooooooon</h2>     
+ <button onclick="play();">Otro mes wiiiiiiii</button>     
+ </center>
+
+<center>
+ <?php include './Ok/Ok.php';?>   
+</center>
+
+<center>
+ <?php include './fotos.php';?>   
+</center>
+   
+    <div class="relativeContainer">
+        <input type=button value="Si" onClick=msg()>
+    </div>
+
+    <div class="relativeContainer">
+        <button id="botonQueSeMueve">
+            No
+        </button>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script>
+        $("#botonQueSeMueve").hover(function () {
+            $("#botonQueSeMueve").css("top", `${Math.random() * 500}px`);
+            $("#botonQueSeMueve").css("left", `${Math.random() * 800}px`);
+        });
+    </script>
+
+</body>
+</html>
